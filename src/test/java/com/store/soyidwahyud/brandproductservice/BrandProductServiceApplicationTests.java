7e97292d5dc9798ecb9d4025cb6f83383ead456a
soyidwahyud <soyidwahyu4@gmail.com>
2022-07-30 14:31:17 +0700

    Update Configuration

--- src/test/java/com/store/soyidwahyud/brandproductservice/BrandProductServiceApplicationTests.java
+++ src/test/java/com/store/soyidwahyud/brandproductservice/BrandProductServiceApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BrandProductServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
