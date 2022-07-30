25103ba0a4874a43665f6ca5fff56971ac966fe3
soyidwahyud <soyidwahyu4@gmail.com>
2022-07-30 13:12:04 +0700

    First commit

---
+++ src/main/java/com/store/soyidwahyud/brandproductservice/BrandProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.store.soyidwahyud.brandproductservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrandProductServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrandProductServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/store/soyidwahyud/brandproductservice/BrandProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.store.soyidwahyud.brandproductservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrandProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
